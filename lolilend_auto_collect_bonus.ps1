cbcde32ba157f7c3140fa38aad96409e0317d035
DemiDge <125759849+DemiDge@users.noreply.github.com>
2026-06-08 21:20:22 +0300

    Update lolilend_auto_collect_bonus.ps1

--- lolilend_auto_collect_bonus.ps1
+++ lolilend_auto_collect_bonus.ps1
@@ -1,13 +1,13 @@
 $headers = @{
     "accept" = "*/*"
-    "access-id" = "01887979-9a2f-7a20-b7f6-f2eb0ab21e80"
-    "access-token" = "977641b99da52aa77325d3af8c6eb80e0cfb60d22cc8aed716b445fead4d83d0"
+    "access-id" = ""
+    "access-token" = ""
     "referer" = "https://loliland.ru/ru/cabinet/bonus"
 }
 
 $cookies = @{
-    "access_id" = "01887979-9a2f-7a20-b7f6-f2eb0ab21e80"
-    "access_token" = "977641b99da52aa77325d3af8c6eb80e0cfb60d22cc8aed716b445fead4d83d0"
+    "access_id" = ""
+    "access_token" = ""
 }
 
 $response = Invoke-WebRequest -Uri "https://loliland.ru/apiv2/bonus/give" `
